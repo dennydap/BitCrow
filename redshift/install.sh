78c6f1b015b4368bd4e5a00e216f9e8c61979882
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2016-11-14 22:31:12 +0000

    changed configurations for redshift

--- redshift/install.sh
+++ redshift/install.sh
@@ -7,7 +7,7 @@ sudo apt-get install redshift
 sudo bash -c "cat > ~/.config/autostart/redshift.desktop << _EOF_
 [Desktop Entry]
 Name=RedShift
-Exec=redshift -l 65:19 -t 5700:3600 -g 0.8 -m randr -v
+Exec=redshift -l 65:19 -t 6400:1000
 Terminal=false
 X-GNOME-Autostart-enabled=true
 _EOF_"
