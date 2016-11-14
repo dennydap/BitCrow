9f96a3e762d15224d4aee9cdbf6271badbea3430
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2016-11-14 22:37:34 +0000

    update

--- redshift/install.sh
+++ redshift/install.sh
@@ -7,8 +7,12 @@ sudo apt-get install redshift
 sudo bash -c "cat > ~/.config/autostart/redshift.desktop << _EOF_
 [Desktop Entry]
 Name=RedShift
+Comment=To help you sleep
 Exec=redshift -l 65:19 -t 6400:1000
+Icon=icon.svg
 Terminal=false
+Type=Application
+Categories=Utility
 X-GNOME-Autostart-enabled=true
 _EOF_"
 sudo chmod +x ~/.config/autostart/redshift.desktop
