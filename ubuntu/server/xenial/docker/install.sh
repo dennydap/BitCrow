2fd60664b556ba1448b3517ab61528f4b94e6c26
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2017-01-21 13:59:24 +0000

    ubuntu server xenial

--- ubuntu/server/xenial/docker/install.sh
+++ ubuntu/server/xenial/docker/install.sh
@@ -4,7 +4,7 @@ set -e
 
 sudo apt-get -y update
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-add-repository -y 'deb https://apt.dockerproject.org/repo ubuntu-trusty main'
+sudo apt-add-repository -y 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 sudo apt-get -y update
 sudo apt-get -y install docker-engine
 
