01fd20b9ca1c04df2a15d89ea3180b9c5a05615f
Hrafn Orri Hrafnkelsson <HrafnOrri1207@gmail.com>
2017-02-01 15:49:28 +0000

    git server install

--- ubuntu/server/xenial/git/install.sh
+++ ubuntu/server/xenial/git/install.sh
@@ -15,18 +15,18 @@ if [ ! -f ~/.ssh/id_rsa ]
     ssh-keygen -t rsa -b 4096 -C "$email"
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa
+    cat ~/.ssh/id_rsa.pub
 fi
 
 installgitlfs=$(cat $(dirname $0)/installgitlfs.config)
 if [ $installgitlfs == "y" ]
   then
-    wget https://github.com/github/git-lfs/releases/download/v1.4.4/git-lfs-linux-amd64-1.4.4.tar.gz
-    tar -zxvf git-lfs-linux-amd64-1.4.4.tar.gz
-    rm git-lfs-linux-amd64-1.4.4.tar.gz
-    chmod +x git-lfs-1.4.4/install.sh
-    cd git-lfs-1.4.4 && sudo ./install.sh && cd ..
-    sudo rm -rf git-lfs-1.4.4
-    sudo git lfs install
+    wget https://github.com/github/git-lfs/releases/download/v1.5.5/git-lfs-linux-amd64-1.5.5.tar.gz
+    tar -zxvf git-lfs-linux-amd64-1.5.5.tar.gz
+    rm git-lfs-linux-amd64-1.5.5.tar.gz
+    chmod +x git-lfs-1.5.5/install.sh
+    cd git-lfs-1.5.5 && sudo ./install.sh && cd ..
+    sudo rm -rf git-lfs-1.5.5
 fi
 
 exit 0
